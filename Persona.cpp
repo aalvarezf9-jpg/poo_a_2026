4158dc84ccec809ae138ff86fb3c5b04dcf3c6f0
aalvarezf9-jpg <aalvarezf9@miumg.edu.gt>
2026-02-15 11:42:36 -0600

    Se agrego clase persona

---
+++ Persona.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class persona{
+//Atributos
+protected:
+	string nombres,apellidos,direccion,fecha_nacimiento;
+	int telefono;
+//Constructores
+	persona(){
+		
+	}
+	persona(string nom, string ape, string dir, string fn, int tel){
+		nombres = nom;
+		apellidos = ape;
+		direccion = dir;
+		telefono = tel;
+		fecha_nacimiento = fn;
+	}
+//Metodo (CRUD)
+	void crear();
+	void leer();
+	void actualizar();
+	void borrar();
+};
